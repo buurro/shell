3d5bdad5e9bca8d85483b3ef939cc0f126346b53
Marco Burro <marcoburro98@gmail.com>
2022-11-01 18:45:06 +0100

    fix slow zsh syntax highlight

--- home.nix
+++ home.nix
@@ -32,17 +32,31 @@
     sudo = "sudo env \"PATH=$PATH\"";
   };
 
-  programs = {
-    zsh = {
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    oh-my-zsh = {
       enable = true;
-      enableAutosuggestions = true;
-      enableSyntaxHighlighting = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "git" "z" ];
-      };
+      plugins = [ "git" "z" ];
     };
+    envExtra = ''
+      ### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+      pasteinit() {
+        OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+        zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+      }
 
+      pastefinish() {
+        zle -N self-insert $OLD_SELF_INSERT
+      }
+      zstyle :bracketed-paste-magic paste-init pasteinit
+      zstyle :bracketed-paste-magic paste-finish pastefinish
+      ### Fix slowness of pastes
+    '';
+  };
+
+  programs = {
     starship.enable = true;
 
     fzf = {
