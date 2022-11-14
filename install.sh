9fd555461781e74d6110eb140f6ed761978b2501
Marco Burro <marcoburro98@gmail.com>
2022-11-14 18:03:37 +0100

    macos install file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-SHELL_REPO="github:buurro/shell"
+SHELL_REPO="github:buurro/shell/main"
 SCREENSHOTS_PATH="$HOME/Pictures/screenshots";
 
 set -e
@@ -6,15 +6,20 @@ set -e
 if [[ $OSTYPE == 'darwin'* ]]; then
     if ! command -v brew &> /dev/null; then
         echo "installing homebrew"
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        /bin/bash <(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
     fi
     if ! command -v nix-env &> /dev/null; then
         echo "installing nix"
         sh <(curl -L https://nixos.org/nix/install)
+        zsh
     fi
 
     if [ ! -d "$SCREENSHOTS_PATH" ]; then
         echo "creating screenshots directory"
         mkdir -p "$SCREENSHOTS_PATH"
     fi
+    FLAKE="$SHELL_REPO#darwinConfigurations.$(hostname -s).system"
+    cd /tmp
+    nix --experimental-features 'nix-command flakes' build "$FLAKE"
+    ./result/sw/bin/darwin-rebuild switch --flake "$SHELL_REPO"
 fi
