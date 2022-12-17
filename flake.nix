86a8efd32b5f47aa0bf7bc01661b7d1f94bce924
Marco Burro <marcoburro98@gmail.com>
2022-12-17 19:57:57 +0100

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,42 +3,49 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    # rust-overlay.url = "github:oxalica/rust-overlay";
+
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager }:
-    let
-      mac-modules = [
-        ./common/darwin-configuration.nix
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.marco = import ./common/home.nix;
-        }
-      ];
-      linux-pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-      darwinConfigurations."smart-kettle" = darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
-        modules = mac-modules;
-      };
-
-      darwinConfigurations."smart-toaster" = darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
-        modules = mac-modules ++ [ ./hosts/smart-toaster/darwin-configuration.nix ];
-      };
-
-      homeConfigurations.common = home-manager.lib.homeManagerConfiguration {
-        pkgs = linux-pkgs;
-        modules = [
-          ./common/home.nix
-          ./common/linux-home.nix
+  outputs = { self, nixpkgs, darwin, home-manager, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        mac-modules = [
+          ./common/darwin-configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            # nixpkgs.overlays = [ rust-overlay.overlays.default ];
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.marco = import ./common/home.nix;
+          }
         ];
-      };
-    };
+      in
+      {
+        packages = {
+          darwinConfigurations."smart-kettle" = darwin.lib.darwinSystem {
+            inherit system;
+            modules = mac-modules;
+          };
+
+          darwinConfigurations."smart-toaster" = darwin.lib.darwinSystem {
+            inherit system;
+            modules = mac-modules ++ [ ./hosts/smart-toaster/darwin-configuration.nix ];
+          };
+
+          homeConfigurations.common = home-manager.lib.homeManagerConfiguration {
+            modules = [
+              ./common/home.nix
+              ./common/linux-home.nix
+            ];
+          };
+        };
+      }
+    );
 }
