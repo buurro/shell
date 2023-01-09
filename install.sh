ca20a88e9db4671623befd5f078d0ecc6479232f
Marco Burro <marcoburro98@gmail.com>
2023-01-09 20:36:02 +0100

    remove sudo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 if [[ $OSTYPE == 'darwin'* ]]; then
     if ! command -v brew &> /dev/null; then
         echo "installing homebrew"
-        sudo NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     if ! command -v nix-env &> /dev/null; then
         echo "installing nix"
