4a08c7b0b7253ae17f07999904ddc4b62b1fa935
Marco Burro <marcoburro98@gmail.com>
2023-03-02 12:07:04 +0100

    add ms word

--- common/darwin-configuration.nix
+++ common/darwin-configuration.nix
@@ -22,6 +22,7 @@ in
       "1Password for Safari" = 1569813296;
       "Magnet" = 441258766;
       "Microsoft Excel" = 462058435;
+      "Microsoft Word" = 462054704;
       "Microsoft Remote Desktop" = 1295203466;
       "Speedtest by Ookla" = 1153157709;
       "Telegram" = 747648890;
