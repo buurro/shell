6c103d079406e90f42bfde5caf3bb78cc4c3ef22
Marco Burro <marcoburro98@gmail.com>
2023-09-08 13:42:15 +0200

    update pc config

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
 
     darwin = {
       url = "github:lnl7/nix-darwin/master";
@@ -14,7 +15,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager, flake-utils }:
+  outputs = { self, nixpkgs, darwin, home-manager, flake-utils, vscode-server }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         mac-modules = [
@@ -43,6 +44,19 @@
             system = "x86_64-linux";
             modules = [
               ./hosts/smart-blender/configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.marco = import ./common/home.nix;
+
+                # Optionally, use home-manager.extraSpecialArgs to pass
+                # arguments to home.nix
+              }
+              vscode-server.nixosModules.default
+              ({ config, pkgs, ... }: {
+                services.vscode-server.enable = true;
+              })
             ];
           };
 
--- hosts/smart-blender/configuration.nix
+++ hosts/smart-blender/configuration.nix
@@ -1,26 +1,51 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  networking.hostName = "smart-blender";
+  networking.firewall.allowedTCPPorts = [
+    6443 # K3s
+  ];
+
+  users.users.marco = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    description = "marco";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    curl
+    git
+    htop
+    vim
+    wget
+    k3s
+  ];
+
+  programs.zsh.enable = true;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services.openssh.enable = true;
+  services.vscode-server.enable = true;
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+  services.k3s.extraFlags = toString [
+    # "--kubelet-arg=v=4" # Optionally add additional args to k3s
+  ];
+
+  # other stuff
+
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "smart-blender"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -76,60 +101,13 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.marco = {
-    isNormalUser = true;
-    description = "marco";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      #  thunderbird
-    ];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
+  # Disable sleep
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "23.05";
 }
