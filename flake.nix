371582c503f5ac3039d2b99256679b099b4d9204
Marco Burro <marcoburro98@gmail.com>
2023-09-09 22:42:59 +0200

    go back

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
     nixos-hardware.url = "github:nixos/nixos-hardware";
