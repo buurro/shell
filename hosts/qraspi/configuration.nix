0a9b3782263d76deaf8937032aa83993c186fc80
Marco Burro <marcoburro98@gmail.com>
2023-09-27 20:25:27 +0200

    remove tv hat

--- hosts/qraspi/configuration.nix
+++ hosts/qraspi/configuration.nix
@@ -4,7 +4,6 @@
 
   networking.firewall.allowedTCPPorts = [
     2049 # NFS
-    9981 # TVHeadend
   ];
 
   environment.systemPackages = with pkgs; [
@@ -12,8 +11,6 @@
     raspberrypi-eeprom
     kmod
   ];
-
-  services.tvheadend.enable = true;
 
   fileSystems."/mnt/sandisk" = {
     device = "/dev/disk/by-uuid/e389b116-e8a1-481b-8b60-334ef44927a8";
@@ -34,8 +31,6 @@
   console.enable = false;
 
   boot = {
-    kernelPackages = lib.mkForce pkgs.linuxPackages_rpi4; # linuxPackages_latest is missing cxd2880 driver
-    initrd.kernelModules = [ "cxd2880" "cxd2880-spi" ];
     loader = {
       generic-extlinux-compatible.enable = lib.mkDefault true;
       grub.enable = lib.mkDefault false;
@@ -48,121 +43,6 @@
       enable = true;
       filter = "*-rpi-4-*.dtb";
       kernelPackage = pkgs.linuxKernel.kernels.linux_rpi4;
-      overlays = [
-        {
-          name = "spi1-2cs"; # using spi0-0cs did not work
-          dtsText = ''
-            /dts-v1/;
-            /plugin/;
-
-
-            / {
-              compatible = "bcm2711";
-
-              fragment@0 {
-                target = <&gpio>;
-                __overlay__ {
-                  spi1_pins: spi1_pins {
-                    brcm,pins = <19 20 21>;
-                    brcm,function = <3>; /* alt4 */
-                  };
-
-                  spi1_cs_pins: spi1_cs_pins {
-                    brcm,pins = <18 17>;
-                    brcm,function = <1>; /* output */
-                  };
-                };
-              };
-
-              fragment@1 {
-                target = <&spi1>;
-                frag1: __overlay__ {
-                  /* needed to avoid dtc warning */
-                  #address-cells = <1>;
-                  #size-cells = <0>;
-                  pinctrl-names = "default";
-                  pinctrl-0 = <&spi1_pins &spi1_cs_pins>;
-                  cs-gpios = <&gpio 18 1>, <&gpio 17 1>;
-                  status = "okay";
-
-                  spidev1_0: spidev@0 {
-                    compatible = "spidev";
-                    reg = <0>;      /* CE0 */
-                    #address-cells = <1>;
-                    #size-cells = <0>;
-                    spi-max-frequency = <125000000>;
-                    status = "okay";
-                  };
-
-                  spidev1_1: spidev@1 {
-                    compatible = "spidev";
-                    reg = <1>;      /* CE1 */
-                    #address-cells = <1>;
-                    #size-cells = <0>;
-                    spi-max-frequency = <125000000>;
-                    status = "okay";
-                  };
-                };
-              };
-
-              fragment@2 {
-                target = <&aux>;
-                __overlay__ {
-                  status = "okay";
-                };
-              };
-
-              __overrides__ {
-                cs0_pin  = <&spi1_cs_pins>,"brcm,pins:0",
-                     <&frag1>,"cs-gpios:4";
-                cs1_pin  = <&spi1_cs_pins>,"brcm,pins:4",
-                     <&frag1>,"cs-gpios:16";
-                cs0_spidev = <&spidev1_0>,"status";
-                cs1_spidev = <&spidev1_1>,"status";
-              };
-            };
-          '';
-        }
-        {
-          name = "rpi-tv";
-          dtsText = ''
-            // rpi-tv HAT
-
-            /dts-v1/;
-            /plugin/;
-
-            / {
-              compatible = "bcm2711";
-
-              fragment@0 {
-                target = <&spidev0>;
-                __overlay__ {
-                  status = "disabled";
-                };
-              };
-
-              fragment@1 {
-                target = <&spi0>;
-                __overlay__ {
-                  /* needed to avoid dtc warning */
-                  #address-cells = <1>;
-                  #size-cells = <0>;
-
-                  status = "okay";
-
-                  cxd2880@0 {
-                    compatible = "sony,cxd2880";
-                    reg = <0>; /* CE0 */
-                    spi-max-frequency = <50000000>;
-                    status = "okay";
-                  };
-                };
-              };
-
-            };
-          '';
-        }
-      ];
     };
   };
 
