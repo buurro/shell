8f145d01466eb3a592e983aad2f42486ebd5f12c
Marco Burro <marcoburro98@gmail.com>
2023-10-26 09:47:35 +0200

    add jellyfin, sonarr, transmission

--- hosts/smart-blender/configuration.nix
+++ hosts/smart-blender/configuration.nix
@@ -20,19 +20,19 @@ in
   ];
 
   networking.hostName = "smart-blender";
+
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [
       # 6443 # Kubernetes API Server
+      # 3389 # RDP
+      # 8443 # Unifi
       80
       443
-      8443 # Unifi
     ];
   };
 
   environment.systemPackages = with pkgs; [
-    lightly-qt
-    ocs-url
     k3s
     chromium
     mpv
@@ -44,6 +44,11 @@ in
     obs-studio
     spotify
     unstablePkgs.vscode.fhs
+
+    # jellyfin hardware acceleration
+    vaapiVdpau
+    libvdpau-va-gl
+    libva-utils
   ];
 
   services.k3s = {
@@ -55,6 +60,18 @@ in
   };
   # systemd.services.k3s.path = [ pkgs.ipset ];
 
+  services.transmission = {
+    enable = true;
+    openRPCPort = true;
+    settings = {
+      download-dir = "/mnt/nas-fun/downloads/complete";
+      incomplete-dir = "/mnt/nas-fun/downloads/incomplete";
+      watch-dir = "/mnt/nas-fun/downloads/watch";
+      watch-dir-enabled = true;
+      speed-limit-up = 100;
+      speed-limit-up-enabled = true;
+    };
+  };
 
   security.acme.acceptTerms = true;
   security.acme.defaults = {
@@ -72,26 +89,84 @@ in
     recommendedTlsSettings = true;
   };
 
+  security.acme.certs."blender.marco.ooo" = { };
+  services.nginx.virtualHosts."blender.marco.ooo" = {
+    forceSSL = true;
+    useACMEHost = "blender.marco.ooo";
+    default = true;
+    locations."/" = {
+      root = "${pkgs.nginx}/html";
+    };
+  };
+
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+    user = "nas";
+    group = "users";
+    dataDir = "/mnt/nas-od/sonarr/config";
+  };
+  security.acme.certs."sonarr.pine.marco.ooo" = { };
+  services.nginx.virtualHosts."sonarr.pine.marco.ooo" = {
+    forceSSL = true;
+    useACMEHost = "sonarr.pine.marco.ooo";
+    locations."/" = {
+      proxyPass = "http://localhost:8989";
+      proxyWebsockets = true;
+    };
+  };
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+  security.acme.certs."jellyfin.pine.marco.ooo" = { };
+  services.nginx.virtualHosts."jellyfin.pine.marco.ooo" = {
+    forceSSL = true;
+    useACMEHost = "jellyfin.pine.marco.ooo";
+    locations."/" = {
+      proxyPass = "http://localhost:8096";
+      proxyWebsockets = true;
+    };
+  };
+
   services.unifi = {
     enable = true;
     openFirewall = true;
     unifiPackage = customUnifi;
-    # jrePackage = unstablePkgs.jdk;
+  };
+  services.nginx.virtualHosts."unifi.pine.marco.ooo" = {
+    listen = [
+      {
+        addr = "0.0.0.0";
+        port = 80;
+        ssl = false;
+      }
+      {
+        addr = "0.0.0.0";
+        port = 443;
+        ssl = true;
+      }
+    ];
+    extraConfig = ''
+      location /wss/ {
+        proxy_pass https://localhost:8443;
+        proxy_http_version 1.1;
+        proxy_buffering off;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_read_timeout 86400;
+      }
+
+      location / {
+        proxy_pass https://localhost:8443/; # The Unifi Controller Port
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+      }
+    '';
   };
 
-  fileSystems."/mnt/nas-fun" = {
-    device = "home-nas:/volume1/fun";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" ];
-  };
-
-  fileSystems."/mnt/nas-od" = {
-    device = "home-nas:/volume1/od";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" ];
-  };
-
-  programs.steam.enable = true;
   services.vscode-server.enable = true;
 
   services.borgbackup.jobs.${config.networking.hostName} = {
@@ -106,17 +181,48 @@ in
     startAt = "daily";
   };
 
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    polkitPolicyOwners = [ "marco" ];
+  programs = {
+    _1password.enable = true;
+    _1password-gui = {
+      enable = true;
+      polkitPolicyOwners = [ "marco" ];
+    };
+    steam.enable = true;
+  };
+
+  fileSystems."/mnt/nas-fun" = {
+    device = "home-nas:/volume1/fun";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" ];
+  };
+
+  fileSystems."/mnt/nas-od" = {
+    device = "home-nas:/volume1/od";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" ];
+  };
+
+  fileSystems."/media/anime" = {
+    depends = [ "/mnt/nas-fun" ];
+    device = "/mnt/nas-fun/media/anime";
+    fsType = "none";
+    options = [ "bind" ];
+  };
+
+  users.users."nas" = {
+    uid = 1024;
+    isSystemUser = true;
+    group = "users";
+  };
+
+  users.users."vm" = {
+    isNormalUser = true;
+    shell = "${inputs.self.images.vm}/bin/run-nixos-vm";
+    openssh.authorizedKeys.keys = config.users.users.marco.openssh.authorizedKeys.keys;
   };
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "armv6l-linux" ];
 
-  # other stuff
-
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
