fec8b59dc4aff77ea2b118038d2aab7aa8175a53
Marco Burro <marcoburro98@gmail.com>
2023-11-02 14:13:15 +0100

    remove caches

--- common/home.nix
+++ common/home.nix
@@ -6,13 +6,9 @@
     substituters = [
       "https://cache.nixos.org"
       "https://cache.nixos.org/"
-      "https://nixpkgs-python.cachix.org"
-      "https://fossar.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-      "fossar.cachix.org-1:Zv6FuqIboeHPWQS7ysLCJ7UT7xExb4OE8c4LyGb5AsE="
     ];
   };
 
