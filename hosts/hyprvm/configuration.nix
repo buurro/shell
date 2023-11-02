b20fbcffc77d346b631a35ecf62e0874fbc1a9ca
Marco Burro <marcoburro98@gmail.com>
2023-11-02 17:47:33 +0100

    fix vm options

--- hosts/hyprvm/configuration.nix
+++ hosts/hyprvm/configuration.nix
@@ -6,10 +6,12 @@
 
   networking.hostName = "hyprvm";
 
-  virtualisation.qemu.options = [
-    "-device virtio-vga-gl"
-    "-display sdl,gl=on,show-cursor=off"
-  ];
+  virtualisation.vmVariant = {
+    virtualisation.qemu.options = [
+      "-device virtio-vga-gl"
+      "-display sdl,gl=on,show-cursor=off"
+    ];
+  };
 
   system.stateVersion = "23.05";
 }
