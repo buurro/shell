b9e7dacf043f5dbd8fe0fcfdc17bdbf268adc8f8
Marco Burro <marcoburro98@gmail.com>
2024-02-09 17:10:54 +0100

    grub theme

--- hosts/burro-hp/configuration.nix
+++ hosts/burro-hp/configuration.nix
@@ -64,6 +64,18 @@
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
 
+  boot.loader.grub.theme =
+    let
+      catppuccin-grub = pkgs.fetchFromGitHub ({
+        owner = "catppuccin";
+        repo = "grub";
+        rev = "803c5df0e83aba61668777bb96d90ab8f6847106";
+        hash = "sha256-/bSolCta8GCZ4lP0u5NVqYQ9Y3ZooYCNdTwORNvR7M0=";
+      });
+    in
+    "${catppuccin-grub}/src/catppuccin-macchiato-grub-theme/"
+  ;
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   nixpkgs.config.allowUnfree = true;
