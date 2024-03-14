faabf75dc472b5c7b886de837b379d6bfe26bfc4
Marco Burro <marcoburro98@gmail.com>
2024-03-14 22:35:52 +0100

    trying k3s

--- hosts/ionos-m/configuration.nix
+++ hosts/ionos-m/configuration.nix
@@ -1,4 +1,4 @@
-{ modulesPath, config, lib, pkgs, ... }: {
+{ pkgs, ... }: {
   imports = [
     ./disk-config.nix
     ./hardware-configuration.nix
@@ -6,6 +6,32 @@
   ];
 
   networking.hostName = "ionos-m";
+
+  environment.systemPackages = [ pkgs.k3s ];
+
+  networking.firewall.allowedTCPPorts = [ 6443 ];
+
+  services.tailscale.enable = true;
+
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+
+  systemd.services.argocd = {
+    requires = [ "k3s.service" ];
+    after = [ "k3s.service" ];
+    serviceConfig =
+      let
+        argoscript = pkgs.writeShellScriptBin "argocd" ''
+          set -e
+
+          ${pkgs.k3s}/bin/k3s kubectl create namespace argocd
+          ${pkgs.k3s}/bin/k3s kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+        '';
+      in
+      {
+        ExecStart = "${argoscript}/bin/argocd";
+      };
+  };
 
   modules.home-manager.enable = true;
 
