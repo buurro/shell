48dc47a5ce48a5b2f0b9077484cd7f6c3dba6f3c
Marco Burro <marcoburro98@gmail.com>
2024-03-28 09:38:26 +0100

    podman

--- hosts/burro-hp/configuration.nix
+++ hosts/burro-hp/configuration.nix
@@ -55,10 +55,21 @@ in
     vscode.fhs
     kitty
     dbeaver
+    podman-compose
   ];
 
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+
+  virtualisation.podman = {
+    enable = true;
+
+    # Create a `docker` alias for podman, to use it as a drop-in replacement
+    dockerCompat = true;
+
+    # Required for containers under podman-compose to be able to talk to each other.
+    defaultNetwork.settings.dns_enabled = true;
+  };
 
   services.xrdp.enable = true;
   services.xrdp.defaultWindowManager = "startplasma-x11";
