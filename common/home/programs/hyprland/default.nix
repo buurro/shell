ca384f6adc97a30c6748cd01afeaeab7c3e9f427
Marco Burro <marcoburro98@gmail.com>
2024-04-24 18:56:22 +0200

    portal tests

--- common/home/programs/hyprland/default.nix
+++ common/home/programs/hyprland/default.nix
@@ -26,7 +26,6 @@
   xdg.portal.enable = true;
   xdg.portal.configPackages = [ pkgs.xdg-desktop-portal-hyprland ];
   xdg.portal.extraPortals = [
-    pkgs.xdg-desktop-portal-hyprland
     pkgs.xdg-desktop-portal-gtk
   ];
 
