6d2f1dee744701bd895ff4b889d14450c1e5de23
Marco Burro <marcoburro98@gmail.com>
2024-08-09 01:12:10 +0200

    hyprland cachix

--- flake.nix
+++ flake.nix
@@ -34,7 +34,14 @@
       url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     };
   };
-
+  nixConfig = {
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
   outputs =
     { self
     , nixpkgs
