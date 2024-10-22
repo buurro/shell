3f736d76068127d510720896b36724cb9eb08ee5
Marco Burro <marcoburro98@gmail.com>
2024-10-22 18:31:07 +0200

    fix qraspi

--- hosts/qraspi/configuration.nix
+++ hosts/qraspi/configuration.nix
@@ -38,11 +38,6 @@
     };
   };
 
-  # This causes an overlay which causes a lot of rebuilding
-  environment.noXlibs = lib.mkForce false;
-  # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix" creates a
-  # disk with this label on first boot. Therefore, we need to keep it. It is the
-  # only information from the installer image that we need to keep persistent
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
