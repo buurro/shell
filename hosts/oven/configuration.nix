67dc0c9f17fb3ac21a05d2cc3197bb854c749139
Marco Burro <marcoburro98@gmail.com>
2024-10-27 08:56:42 +0100

    cheshire cat

--- hosts/oven/configuration.nix
+++ hosts/oven/configuration.nix
@@ -34,6 +34,23 @@
     openFirewall = true;
   };
 
+  networking.firewall.allowedTCPPorts = [
+    80
+    5678
+  ];
+
+  virtualisation.oci-containers.containers = {
+    cheshire_cat_core = {
+      image = "ghcr.io/cheshire-cat-ai/core:1.7.1";
+      extraOptions = [ "--network=host" ];
+      volumes = [
+        "cheshire_cat_static:/app/cat/static"
+        "cheshire_cat_plugins:/app/cat/plugins"
+        "cheshire_cat_data:/app/cat/data"
+      ];
+    };
+  };
+
   modules.hyprland.enable = true;
   modules.home-manager.enable = true;
 
