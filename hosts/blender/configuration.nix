8ac00042585ce7bda305d76de4247303a6001142
Marco Burro <marcoburro98@gmail.com>
2024-11-04 22:26:24 +0100

    k3s

--- hosts/blender/configuration.nix
+++ hosts/blender/configuration.nix
@@ -22,6 +22,7 @@ in
     allowedTCPPorts = [
       80
       443
+      2222
       6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
       # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
       # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
@@ -68,13 +69,13 @@ in
     ];
   };
 
-  # services.k3s = {
-  #   enable = true;
-  #   role = "server";
-  #   extraFlags = toString [
-  #     "--disable=traefik"
-  #   ];
-  # };
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      "--disable=traefik"
+    ];
+  };
 
   services.transmission = {
     enable = true;
