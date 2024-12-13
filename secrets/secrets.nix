95e0d633dc9d34eb709e5f2144ba563d17d71140
Marco Burro <marcoburro98@gmail.com>
2024-12-13 20:23:50 +0100

    secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,10 +3,11 @@ let
   qraspi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUnZQFo+GMqhnNUfzExwZvZRPmOy8+bZyABQCsSyqT0";
   mixer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhOcZVqmRV4SLWJQX73zb6j+e9V3bPlHaTyovmNXioJ";
   toaster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwYNiDUMF/rOBh929JDGXtr5371osQkgHAa7pmiuTwr";
+  other = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIR/Dqd+UXeEQovChEHgDhIIaXcrpa+i2/KwECTbkp5q";
 in
 {
   "authelia.jwtSecretFile.age".publicKeys = [ blender ];
   "authelia.storageEncryptionKeyFile.age".publicKeys = [ blender ];
-  "risaro.la.age".publicKeys = [ blender qraspi ];
-  "mixer-vpn-conf.age".publicKeys = [ toaster mixer ];
+  "risaro.la.age".publicKeys = [ qraspi toaster other ];
+  "somefile.zip.age".publicKeys = [ mixer toaster other ];
 }
