358a0e921324af659b1210077a6916faf6d09047
Marco Burro <marcoburro98@gmail.com>
2025-02-23 10:24:44 +0100

    fix darwin error

--- common/darwin-configuration.nix
+++ common/darwin-configuration.nix
@@ -115,10 +115,6 @@ in
 
   security.pam.enableSudoTouchIdAuth = true;
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
