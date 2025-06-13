51de7a08f2d97edd3708e19b4e2e7571bbc1f257
Marco Burro <marcoburro98@gmail.com>
2025-06-13 11:18:19 +0200

    revert "wooper podman"
    This reverts commit 4bdf208e14d465cb41204f4551a80903b6b9caf1.

--- hosts/wooper/configuration.nix
+++ hosts/wooper/configuration.nix
@@ -8,25 +8,8 @@
 
   networking.firewall.allowedTCPPorts = [ 6443 ];
 
-  environment.systemPackages = with pkgs; [
-    k3s
-    dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    docker-compose # start group of containers for dev
-  ];
+  environment.systemPackages = [ pkgs.k3s ];
 
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
 
   services.k3s.enable = true;
   services.k3s.role = "server";
