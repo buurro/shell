4bdf208e14d465cb41204f4551a80903b6b9caf1
Marco Burro <marcoburro98@gmail.com>
2025-06-13 11:15:20 +0200

    wooper podman

--- hosts/wooper/configuration.nix
+++ hosts/wooper/configuration.nix
@@ -8,8 +8,25 @@
 
   networking.firewall.allowedTCPPorts = [ 6443 ];
 
-  environment.systemPackages = [ pkgs.k3s ];
+  environment.systemPackages = with pkgs; [
+    k3s
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    docker-compose # start group of containers for dev
+  ];
 
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 
   services.k3s.enable = true;
   services.k3s.role = "server";
